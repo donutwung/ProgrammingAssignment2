5b7cf13db379f4659f69cb9d17ea602a383f132a
donutwung <77430906+donutwung@users.noreply.github.com>
2021-01-15 14:49:31 +0700

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that create an object that stores a
+## matrix and cache's its inverse
 
 ## Write a short comment describing this function
 

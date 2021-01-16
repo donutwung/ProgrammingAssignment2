e311a032d6554fd72cd9c56987ac12b3cd5d273f
donutwung <77430906+donutwung@users.noreply.github.com>
2021-01-16 20:55:42 +0700

    CacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,35 +1,27 @@
-## Below are two functions that create an object that stores a
-## matrix and cache's its inverse
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  z <-NULL
-  set <- function(y) {
-    x <<- y
-    z <<- NULL
-  }
-  get <- function() x
-  setinv <- function(inv) m <<- inv
-  getinv <- function () m
-  list(set = set, get = get,
-       setinv = setinv,
-       getinv = getinv)
-  
+makeCacheMatrix <- function(x = matrix()){
+    invrs <- NULL
+    set <- function(y){
+        x <<- y 
+        invrs <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) invrs <<- solve         
+    getsolve <- function() invrs
+    list( set = set,
+          get = get ,
+          setsolve = setsolve ,
+          getsolve = getsolve
+    )
+    
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-  z <- x$getinv()
-  if(!is.null(z)) {
-    message("getting cached data")
-    return(z)
-  }
-  data <- x$get()
-  z <- solve(data, ...)
-  x$setinv(z)
-  z
-  ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+    invrs <- x$getsolve()
+    if (!is.null(invrs)){
+        message("getting cached data")
+        return(invrs)
+    }
+    data <- x$get()
+    invrs <- solve(data,...)
+    x$setsolve(invrs)
+    invrs
 }
